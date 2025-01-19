f42f8a664d8b11859f26ce6e155cc409c2e69790
gustavo <gustavo.isaac@gmail.com>
2025-01-19 14:39:56 +0000

    CaptchaConstants

---
+++ app/src/main/java/com/artemissoftware/beerace/feature/recaptcha/presentation/util/CaptchaConstants.kt
@@ -0,0 +1,5 @@
+package com.artemissoftware.beerace.feature.recaptcha.presentation.util
+
+object CaptchaConstants {
+    const val URL_VERIFY_CAPTCHA_SUCCESS = "https://www.google.com/recaptcha/api2/userverify"
+}
