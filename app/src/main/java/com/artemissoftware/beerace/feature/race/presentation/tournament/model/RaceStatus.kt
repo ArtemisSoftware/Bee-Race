36a20eafa0cf444ac3f79a2ea6f8e600fee66d6a
gustavo <gustavo.isaac@gmail.com>
2025-01-19 13:54:34 +0000

    RaceStatus

---
+++ app/src/main/java/com/artemissoftware/beerace/feature/race/presentation/tournament/model/RaceStatus.kt
@@ -0,0 +1,9 @@
+package com.artemissoftware.beerace.feature.race.presentation.tournament.model
+
+enum class RaceStatus {
+    RUNNING,
+    PAUSED,
+    NOT_STARTED,
+    FINISHED,
+    INTERRUPTED
+}
