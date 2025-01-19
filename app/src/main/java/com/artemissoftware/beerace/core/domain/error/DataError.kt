80b84d95ecbb0acb0acda0a39f22387f68900b57
gustavo <gustavo.isaac@gmail.com>
2025-01-19 11:56:01 +0000

    DataError

---
+++ app/src/main/java/com/artemissoftware/beerace/core/domain/error/DataError.kt
@@ -0,0 +1,13 @@
+package com.artemissoftware.beerace.core.domain.error
+
+sealed interface DataError : Error {
+
+    sealed class NetworkError : DataError {
+        data class Error(val message: String) : NetworkError()
+        data class CaptchaControl(val url: String) : NetworkError()
+        data object SocketTimeout : NetworkError()
+        data object Unknown : NetworkError()
+        data object UnknownHost : NetworkError()
+        data object Connect : NetworkError()
+    }
+}
